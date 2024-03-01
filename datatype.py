d53a75702b0bad354eabec0b185028c5816a2272
sonam choden <sonamchoden506tt@gmail.com>
2024-02-29 22:57:21 -0800

    conditional and indintation

---
+++ datatype.py
@@ -0,0 +1,10 @@
+variable1 = 10 #integer
+
+print(variable1)
+name ='dechen' #string
+name2 = "sonam"
+score = 10.1 #floAT / DECIMALS
+didHeWin = True #boolean
+# ...
+print(type(variable1))
+print(type(name))

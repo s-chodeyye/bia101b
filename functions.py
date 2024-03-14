2212a18d366d7fa68f4b9fbe36533633fed9f98c
sonam choden <sonamchoden506tt@gmail.com>
2024-03-14 02:20:08 -0700

    functions

---
+++ functions.py
@@ -0,0 +1,23 @@
+def greet() :
+    # do some calculation code
+    a= 1 +2
+    print (a)
+    # complex stuffs
+    print('Hello there')
+greet()
+
+user_input = 'Sonam'
+if user_input == 'nima':
+    greet_nima
+
+
+def addnumbers(num1, num2):
+    sum = num1 + num2
+    print('adding numbers')
+    print(sum)
+
+usernumber = 10
+usernumber2 = 20
+addnumbers(usernumber, usernumber2)
+
+
